ad6877817f691b734ee48ce7d71377d8a733c921
bolian <1298156652@qq.com>
2019-09-07 18:31:43 +0800

    请求参数的绑定和获取Cookes和请求转发

--- springmvc_01/src/main/java/cn/bolianlai/handler/HelloHandler.java
+++ springmvc_01/src/main/java/cn/bolianlai/handler/HelloHandler.java
@@ -1,14 +1,46 @@
 package cn.bolianlai.handler;
 
+import cn.bolianlai.entity.User;
+import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloHandler {
 
-    @RequestMapping("/index")
-    public String index() {
-        System.out.println("执行业务方法index");
+//    参数绑定
+    @RequestMapping("index")
+    public String index(@RequestParam("name") String name, @RequestParam("id") int id) {
+        System.out.println("执行业务方法index:"+ name + id);
         return "index";
+    }
+
+//    RESTful 风格参数
+    @RequestMapping("rest/{name}/{id}")
+    public String rest(@PathVariable("name") String name, @PathVariable("id") String id) {
+        System.out.println("执行业务方法rest:"+ name + id);
+        return "index";
+    }
+
+//    映射 Cookie
+    @RequestMapping("cookie")
+    public String cookie(@CookieValue("JSESSIONID") String sessionId) {
+        System.out.println("执行业务方法cookie:" + sessionId);
+        return "index";
+    }
+//    使用 POJO 绑定参数
+    @RequestMapping("addUser")
+    public String getPOJO(User user){
+        System.out.println("执行业务方法getPOJO:" + user);
+        return "index";
+    }
+
+//    JSP页面的转发和重定向
+    @RequestMapping("redirect")
+    public String redirect() {
+        return "redirect:cookie";
     }
 }
---
+++ springmvc_01/src/main/webapp/addUser.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 12981
+  Date: 2019/9/7
+  Time: 18:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="addUser" method="post">
+    编号：<input type="text" name="id"/><br/>
+    姓名：<input type="text" name="name"/><br/>
+    地址：<input type="text" name="address.name"/><br/>
+    <input type="submit" value="提交"/>
+</form>
+</body>
+</html>
